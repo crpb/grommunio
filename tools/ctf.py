9cd5c3973dfa15a11e32552cdc45661c532b8b08
cb <christopher@bocki.com>
2023-06-16 01:00:16 +0200

    blacked..

--- tools/ctf.py
+++ tools/ctf.py
@@ -10,51 +10,48 @@ __license__ = "MIT License (Expat)"
 __doc__ = "Capture all those Flags!!!!"
 
 parser = argparse.ArgumentParser(description=__doc__)
-group = parser.add_argument_group('filters')
-group.add_argument('--years', metavar='N', type=int,
-                   default=1, help='years in the past')
-group.add_argument('--flagged', action=argparse.BooleanOptionalAction,
-                   default=True)
-group = parser.add_argument_group('options')
-group.add_argument('--mark', action=argparse.BooleanOptionalAction,
-                   default=False)
-group.add_argument('--limit', metavar='N', type=int,
-                   default=50)
-group.add_argument('--unflag', action="store_true")
-group = parser.add_argument_group('imap')
-group.add_argument('server')
-group.add_argument('username')
-group.add_argument('password')
+group = parser.add_argument_group("filters")
+group.add_argument(
+    "--years", metavar="N", type=int, default=1, help="years in the past"
+)
+group.add_argument("--flagged", action=argparse.BooleanOptionalAction, default=True)
+group = parser.add_argument_group("options")
+group.add_argument("--mark", action=argparse.BooleanOptionalAction, default=False)
+group.add_argument("--limit", metavar="N", type=int, default=50)
+group.add_argument("--unflag", action="store_true")
+group = parser.add_argument_group("imap")
+group.add_argument("server")
+group.add_argument("username")
+group.add_argument("password")
 args = parser.parse_args()
 
 dt = datetime.now()
-dt = dt.replace(year=dt.year-args.years)
+dt = dt.replace(year=dt.year - args.years)
 
 """
 Iterate over all Folders
 Look for [Not]Flagged Messages
 and whatever...
 """
-crit=AND(flagged=args.flagged, date_lt=dt.date())
+crit = AND(flagged=args.flagged, date_lt=dt.date())
 if args.unflag:
-    print(f'Mailbox: {args.username}')
+    print(f"Mailbox: {args.username}")
     with MailBox(args.server).login(args.username, args.password) as mailbox:
         for f in mailbox.folder.list():
-            print(f'\t{f.name}')
+            print(f"\t{f.name}")
             mailbox.folder.set(f.name)
-            FLAGS = (MailMessageFlags.SEEN)
-            print('\t\tSET SEEN')
+            FLAGS = MailMessageFlags.SEEN
+            print("\t\tSET SEEN")
             mailbox.flag(mailbox.uids(crit), FLAGS, True)
-            FLAGS = (MailMessageFlags.FLAGGED)
-            print('\t\tUNFLAGGING')
+            FLAGS = MailMessageFlags.FLAGGED
+            print("\t\tUNFLAGGING")
             mailbox.flag(mailbox.uids(crit), FLAGS, False)
-#If we don't want to take action we will see what we would have matched
+# If we don't want to take action we will see what we would have matched
 else:
     with MailBox(args.server).login(args.username, args.password) as mailbox:
         for f in mailbox.folder.list():
             mailbox.folder.set(f.name)
-            for msg in mailbox.fetch(mark_seen=args.mark,
-                                     criteria=crit,
-                                     limit=args.limit):
-                print(f.name, msg.date, msg.subject[0:30],
-                      len(msg.text or msg.html))
+            for msg in mailbox.fetch(
+                mark_seen=args.mark, criteria=crit, limit=args.limit
+            ):
+                print(f.name, msg.date, msg.subject[0:30], len(msg.text or msg.html))
--- tools/midb_repair.py
+++ tools/midb_repair.py
@@ -20,13 +20,13 @@ import warnings
 
 # we still have a little time left until python3.13!
 with warnings.catch_warnings():
-    warnings.filterwarnings("ignore",category=DeprecationWarning)
+    warnings.filterwarnings("ignore", category=DeprecationWarning)
     import telnetlib
 
 #  # jwilk/python-syntax-errors
 #  lambda x, /: 0  # Python >= 3.8 is required
 if not sys.version_info >= (3, 8):
-    print('Required version Python 3.8')
+    print("Required version Python 3.8")
     sys.exit()
 
 pp = pprint.PrettyPrinter()
@@ -35,24 +35,43 @@ __doc__ = "Regenerate the MIDB of Gromox-Mailboxes"
 
 parser = argparse.ArgumentParser(description=__doc__)
 group = parser.add_mutually_exclusive_group(required=True)
-group.add_argument('--all', action='store_true', default=True, help='All Mailboxes')
-group.add_argument('--user', action='store', help='Username of Mailbox, e.g. user@dom.tld')
-#group.add_argument('--file', action='store', help='Filepath to Userlist')
-group = parser.add_argument_group('options')
-group.add_argument('--backup', action=argparse.BooleanOptionalAction, default=False, help='Backup MIDB')
-group.add_argument('--recreate', action=argparse.BooleanOptionalAction, default=True, help='Recreate MIDBs')
-group.add_argument('--dry-run', action=argparse.BooleanOptionalAction, default=True, help='Only show what would happen')
-group.add_argument('--verbose', action=argparse.BooleanOptionalAction, default=False, help='Some noise')
+group.add_argument("--all", action="store_true", default=True, help="All Mailboxes")
+group.add_argument(
+    "--user", action="store", help="Username of Mailbox, e.g. user@dom.tld"
+)
+# group.add_argument('--file', action='store', help='Filepath to Userlist')
+group = parser.add_argument_group("options")
+group.add_argument(
+    "--backup", action=argparse.BooleanOptionalAction, default=False, help="Backup MIDB"
+)
+group.add_argument(
+    "--recreate",
+    action=argparse.BooleanOptionalAction,
+    default=True,
+    help="Recreate MIDBs",
+)
+group.add_argument(
+    "--dry-run",
+    action=argparse.BooleanOptionalAction,
+    default=True,
+    help="Only show what would happen",
+)
+group.add_argument(
+    "--verbose", action=argparse.BooleanOptionalAction, default=False, help="Some noise"
+)
 pargs = parser.parse_args()
 
-if pargs.verbose: pp.pprint(pargs)
+if pargs.verbose:
+    pp.pprint(pargs)
+
 
 def trigger(users):
-    if pargs.verbose: print('Running Trigger')
-    telnet_client = telnetlib.Telnet('::1', 5555)
+    if pargs.verbose:
+        print("Running Trigger")
+    telnet_client = telnetlib.Telnet("::1", 5555)
     wcount = 0
     mcount = len(users)
-    telnet_client.read_until(b"OK", timeout=10) # wait for OK
+    telnet_client.read_until(b"OK", timeout=10)  # wait for OK
     print(f"{mcount} mail directories for MIDB synchronization found.")
     for user in users:
         mbx = users[user]
@@ -60,107 +79,110 @@ def trigger(users):
         wcount += 1
         print("Mailbox {:>3}: {}".format(wcount, mbx.strip()))
         # Build the synchronization command
-        byte_obj = ( "X-RSYM " + mbx.strip() + "\r\n").encode("ascii")
+        byte_obj = ("X-RSYM " + mbx.strip() + "\r\n").encode("ascii")
         telnet_client.write(byte_obj)
-        telnet_client.read_until(b"TRUE ", timeout=300) # wait max. 5 minutes for TRUE
+        telnet_client.read_until(b"TRUE ", timeout=300)  # wait max. 5 minutes for TRUE
     print(f"[Closing connection]")
-    telnet_client.write(b'exit')
+    telnet_client.write(b"exit")
+
 
 def backup_midb(users):
-    '''
+    """
     Is actually not really helpful if we run the cleaner afterwards :-) - ~cb
-    '''
-    if pargs.verbose: print(f'Backup: {pargs.backup}')
+    """
+    if pargs.verbose:
+        print(f"Backup: {pargs.backup}")
     for user in users:
         command = f'sqlite3 {users[user]}/exmdb/midb.sqlite3 ".backup {users[user]}/exmdb/midb.bak.sqlite3"'
-        if pargs.verbose: print(command)
+        if pargs.verbose:
+            print(command)
         if not pargs.dry_run:
             subprocess.call(command, shell=True)
 
+
 def recreate_midb(users):
-    if pargs.verbose: print(f'recreate midbs')
-    args = ['systemctl',
-            'stop',
-            'gromox-midb',
-            'gromox-imap',
-            'gromox-pop3'
-            ]
-    if pargs.verbose: print(args)
+    if pargs.verbose:
+        print(f"recreate midbs")
+    args = ["systemctl", "stop", "gromox-midb", "gromox-imap", "gromox-pop3"]
+    if pargs.verbose:
+        print(args)
     if not pargs.dry_run:
         subprocess.call(args)
     for user in users:
-        if pargs.verbose: opts='-vf'
-        else: opts='-f'
-        args = ['gromox-mkmidb',
-                f'{opts}',
-                f'{user}'
-                ]
-        if pargs.verbose: print(args)
+        if pargs.verbose:
+            opts = "-vf"
+        else:
+            opts = "-f"
+        args = ["gromox-mkmidb", f"{opts}", f"{user}"]
+        if pargs.verbose:
+            print(args)
         if not pargs.dry_run:
             subprocess.call(args)
-    args = ['systemctl',
-            'restart',
-            'gromox-midb',
-            'gromox-imap',
-            'gromox-pop3'
-            ]
-    if pargs.verbose: print(args)
+    args = ["systemctl", "restart", "gromox-midb", "gromox-imap", "gromox-pop3"]
+    if pargs.verbose:
+        print(args)
     if not pargs.dry_run:
         subprocess.call(args)
     for user in users:
-        args = ['/usr/libexec/gromox/cleaner',
-                '-d',
-                f'{users[user]}'
-                ]
-        if pargs.verbose: print(args)
+        args = ["/usr/libexec/gromox/cleaner", "-d", f"{users[user]}"]
+        if pargs.verbose:
+            print(args)
         if not pargs.dry_run:
-            print(f'Cleaner: {user}')
+            print(f"Cleaner: {user}")
             subprocess.call(args)
+
 
 if pargs.all:
     data = subprocess.check_output(
-            ['grommunio-admin',
-                'user',
-                'query',
-                'username',
-                'maildir',
-                '--filter',
-                'pop3_imap=True',
-                '--sort',
-                'maildir',
-                '--format',
-                'json-structured'
-                ],
-            universal_newlines=True)
+        [
+            "grommunio-admin",
+            "user",
+            "query",
+            "username",
+            "maildir",
+            "--filter",
+            "pop3_imap=True",
+            "--sort",
+            "maildir",
+            "--format",
+            "json-structured",
+        ],
+        universal_newlines=True,
+    )
 
 if pargs.user:
     data = subprocess.check_output(
-            ['grommunio-admin',
-                'user',
-                'query',
-                'username',
-                'maildir',
-                '--filter',
-                'pop3_imap=True',
-                '--sort',
-                'maildir',
-                '--format',
-                'json-structured',
-                '--filter',
-                f'username={pargs.user}'
-                ],
-            universal_newlines=True)
+        [
+            "grommunio-admin",
+            "user",
+            "query",
+            "username",
+            "maildir",
+            "--filter",
+            "pop3_imap=True",
+            "--sort",
+            "maildir",
+            "--format",
+            "json-structured",
+            "--filter",
+            f"username={pargs.user}",
+        ],
+        universal_newlines=True,
+    )
 
 users = {}
 for user in json.loads(data):
-    users[user['username']] = user['maildir']
+    users[user["username"]] = user["maildir"]
 
-if pargs.verbose: 
-    print('Users')
-    [(print(value, end='\t'), print(key)) for key, value in users.items()]
+if pargs.verbose:
+    print("Users")
+    [(print(value, end="\t"), print(key)) for key, value in users.items()]
 
-if pargs.backup: backup_midb(users)
-if pargs.recreate: recreate_midb(users)
-if not pargs.dry_run: trigger(users)
+if pargs.backup:
+    backup_midb(users)
+if pargs.recreate:
+    recreate_midb(users)
+if not pargs.dry_run:
+    trigger(users)
 
 # vim:ts=4 sts=4 sw=4 et
