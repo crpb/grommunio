fd028ce4ffa0a17613764633edda465883036f4f
Christopher Bock <christopher@bocki.com>
2024-08-23 10:40:14 +0200

    exmdb-perms.sh

---
+++ sql/exmdb-perms.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+QUERY='select username, maildir from users where maildir like "/var/lib/gromox/user/%"'
+if [ $# = 1 ]; then
+  USR="$1"
+  QUERY="$QUERY AND username = \"${USR}\"" 
+fi
+mysql grommunio --skip-column-names \
+ --execute="$QUERY" |
+  while read -r username maildir; do
+    printf '{ "username": "%s", "maildir": "%s", "permissions": %s }' \
+      "$username" "$maildir" \
+      "$(sqlite3 -json $maildir/exmdb/exchange.sqlite3 < /root/scripts/sql/exmdb-perms.sql)";
+    done
