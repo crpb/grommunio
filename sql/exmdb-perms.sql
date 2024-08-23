f6ec27abbf4a37a73306af642346dc2dc066627e
Christopher Bock <christopher@bocki.com>
2024-08-23 09:33:10 +0200

    exmdb-perms.sql

--- sql/exmdb-perms.sql
+++ sql/exmdb-perms.sql
@@ -1,7 +1,7 @@
 -- FOLDER PERMISSIONS LOOKUP TABLE
 CREATE TABLE IF NOT EXISTS "temp"."perms" (
-	"perm"	INTEGER UNIQUE,
-	"name"	TEXT
+  "perm"  INTEGER UNIQUE,
+  "name"  TEXT
 );
 INSERT OR REPLACE INTO perms("perm","name") VALUES
 ( 1,'redany'),
@@ -19,29 +19,47 @@ INSERT OR REPLACE INTO perms("perm","name") VALUES
 ( 4096,'freebusydetailed'),
 ( 8192,'storeowner')
 ;
--- FOLDER LOOKUP VIEW
-CREATE VIEW IF NOT EXISTS "temp"."folderlist"
-AS SELECT
-	distinct(p.folder_id),
-    f1.propval
-  FROM permissions p
- INNER JOIN folder_properties f1
-    ON f1.folder_id = p.folder_id
-   AND f1.proptag = 805371935
- ORDER BY p.folder_id
+-- FOLDER LOOKUP VIEW VIA FOLDERS INCLUDING PARENT
+CREATE VIEW IF NOT EXISTS temp.folderlist
+AS SELECT distinct(p.folder_id),
+          p.parent_id,
+          f1.propval
+     FROM folders p
+     JOIN folder_properties f1
+       ON f1.folder_id = p.folder_id
+      AND f1.proptag = 805371935
+    ORDER BY p.folder_id
+;
+-- FOLDER LOOKUP VIEW VIA PERMISSIONS
+CREATE VIEW IF NOT EXISTS temp.folderlist_p
+AS SELECT distinct(p.folder_id),
+          f1.propval
+  FROM    permissions p
+ INNER    JOIN folder_properties f1
+    ON    f1.folder_id = p.folder_id
+   AND    f1.proptag = 805371935
+ ORDER    BY p.folder_id
 ;
 -- LOOKUP PERMISSIONS
-  SELECT p.folder_id as folder_int,
-         (SELECT printf('0x%x',p.folder_id)) AS folder_hex,
-         f.propval AS folder_name,
-         p.username,
-         (SELECT printf('0x%x',p.permission)) AS permission_hex,
-         x.name as permission
-    FROM permissions p
-    JOIN folderlist f
-      ON p.folder_id = f.folder_id
-    LEFT JOIN temp.perms x
-      ON x.perm = p.permission
-ORDER BY p.folder_id ASC,
-         p.username ASC
+CREATE VIEW IF NOT EXISTS temp.folderpermissions
+AS SELECT p.folder_id as folder_int,
+          f.parent_id,
+          (SELECT printf('0x%x',p.folder_id)) AS folder_hex,
+          (SELECT printf('0x%x',f.parent_id)) AS parent_hex,
+          f.propval AS folder_name,
+          p.username,
+          x.name as permission,
+          (SELECT printf('0x%x',p.permission)) AS permission_hex
+    FROM  permissions p
+    JOIN  folderlist f
+      ON  p.folder_id = f.folder_id
+    LEFT  JOIN temp.perms x
+      ON  x.perm = p.permission
+ORDER BY  p.folder_id ASC,
+          f.parent_id ASC,
+          p.username ASC
 ;
+-- QUERY OUR VIEW
+SELECT * FROM folderpermissions
+;
+
