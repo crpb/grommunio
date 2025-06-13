b3a127ebc3c3410ae98f6642189b0220a5f736b8
Christopher Bock <christopher@bocki.com>
2025-06-13 10:02:17 +0200

    g_cgkrepair.sh

--- tools/g_cgkrepair.sh
+++ tools/g_cgkrepair.sh
@@ -1,5 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
+#set -e
 #
+set -o pipefail
 # Check or repair all mailboxes with cgkrepair.
 #
 # Script to check or repair ICS properties in all mailboxes. In some cases
@@ -33,104 +35,145 @@
 #
 # Run the script and specify the required parameter, just -f.
 #
-#
-CYA="\033[0;36m"; YEL="\033[0;33m"; NORM="\033[0m" BLUE="\033[0;34m" RED="\033[0;31m"
-log="/tmp/g_cgkrepair.log"
-repair=false
+log='/tmp/g_cgkrepair.log'
 quiet=false
+repair=false
+silent=false
 verbose=false
-dry_run="-n"
+dry_run='-n'
 count=0
 err_dir=0
 errors=0
 #
-OPTIND=1
-while getopts ":fqv" opt; do
-    case $opt in
-        f) repair=true;;
-	q) quiet=true;;
-        v) verbose=true;;
-        *) ;;
+BLUE='\033[0;34m'
+CYA='\033[0;36m'
+NORM='\033[0m'
+RED='\033[0;31m'
+YEL='\033[0;33m'
+
+showhelp() {
+  echo -e "${CYA}Checks or repairs all grommunio mailboxes on this system with cgkrepair.${NORM}"
+  echo -e "  Use the ${YEL}-f${NORM} parameter to start the repair."
+  echo -e "  Without -f the mailbox will only be checked (dry run)."
+  echo -e "  Use ${YEL}-v${NORM} to be verbose."
+  echo -e "  Use ${CYA}-s${NORM} to only show defective mailboxes."
+  echo -e "  Use ${RED}-q${NORM} to be quiet (the log will still be filled)."
+  echo -e "  Use -l path/to/file to use another logfile than the default /tmp/g_ckgrepair.log.\n"
+}
+
+
+#OPTIND=1
+while getopts hfsqve:l: opts
+do
+    case "${opts}" in
+        e)
+            email="${OPTARG:?}"
+            ;;
+        f)
+            repair=true
+            ;;
+        h)
+            showhelp
+            exit 0
+            ;;
+        l)
+            if touch -c "$OPTARG" 2>/dev/null; then
+                log="${OPTARG:?}"
+            else
+                exit 77
+            fi
+            ;;
+        q)
+            quiet=true
+            ;;
+        s)
+            silent=true
+            ;;
+        v)
+            verbose=true
+            ;;
+        ?)
+            showhelp
+            exit 1
+            ;;
     esac
 done
 shift $((OPTIND - 1))
 
+#[ "$#" -eq 0 ] && showhelp
+
 if $quiet; then
-	#exec 1>> >(ts '[%Y-%m-%d %H:%M:%.S]' >> "$log") 2>&1	
-	exec 1>> >(sed -e 's/\x1b\[[0-9;]*m//g' >> "$log") 2>&1
+  #exec 1>> >(ts '[%Y-%m-%d %H:%M:%.S]' >> "$log") 2>&1	
+  exec 3>&1 4>&2 1>/dev/null 2>&4
 fi
 logg() { sed -e 's/\x1b\[[0-9;]*m//g' >> "$log"; }
 
 MSGT="Check"
-MSGL=$MSGT
-echo
-echo -e "${CYA}Checks or repairs all grommunio mailboxes on this system with cgkrepair.${NORM}"
-echo -e "  Use the ${YEL}-f${NORM} parameter to start the repair."
-echo -e "  Without -f the mailbox will only be checked (dry run)."
-echo -e "  Use ${YEL}-v${NORM} to be verbose."
-echo -e "  Use ${RED}-q${NORM} to be quiet (the log will still be filled)."
+
 if $repair; then
-    echo -e "${YEL}-f is set, repair all mailboxes ...${NORM}"
-    MSGT="${YEL}Repair"
-    MSGL="Repair"
-    dry_run=""
+  echo -e "${YEL}-f is set, repair all mailboxes ...${NORM}"
+  MSGT="${YEL}Repair"
+  dry_run=""
 fi
-echo
-echo "Script $0 started at: $(date +%Y%m%d-%H%M%S)" >"$log"
+echo "Script $0 started at: $(date +%Y%m%d-%H%M%S)" >>"$log"
 #
+# If -e usr@dom.tld was supplied
+[ -n "$email" ] && mailboxes=$email
 # Read all mailboxes in a variable
-mailboxes=$(grommunio-admin user query username maildir | grep "/user/" | grep "@" | awk '{print $1}')
+[ -z "$email" ] && mailboxes=$(gromox-mbop foreach.mb echo-username)
 
 # Now iterate through all mailboxes
 # shellcheck disable=SC2068
-for mailbox in ${mailboxes[@]}; do
-    entries=
-    output=
-    maildir=$(gromox-mbop -u "$mailbox" echo-maildir)
-    count=$((count +1))
-    if ! [ -d "$maildir" ]; then
-	    echo "Error: No Maildir/Access: $mailbox" |tee /dev/stderr |tee >(logg)
-	err_dir=$((err_dir +1))
-        continue
-    fi
-    
-    echo -e "$MSGT mailbox # ${count}: ${CYA}$mailbox${NORM} ... " |tee >(logg)
- 
+for mailbox in $mailboxes; do
+  entries=
+  output=
+  maildir=$(gromox-mbop -u "$mailbox" echo-maildir)
+  count=$((count +1))
+  if ! [ -d "$maildir" ]; then
+    echo "Error: No Maildir/Access: $mailbox" |tee /dev/stderr |tee >(logg)
+    err_dir=$((err_dir +1))
+    continue
+  fi
+
+  $silent || echo -e "$MSGT mailbox # ${count}: ${CYA}$mailbox${NORM} ... " |tee >(logg)
+
     # shellcheck disable=SC2086
     output=$(/usr/libexec/gromox/cgkrepair $dry_run -e "$mailbox" | tee -a "$log")
-    if [[ $? -ne 0 ]]; then
-        errors=$((errors +1))
-        echo -e "${RED}Error found!${NORM}" |tee /dev/stderr |tee >(logg)
+    ret=$?
+    if [[ $ret -ne 0 ]]; then
+      errors=$((errors +1))
+      echo -e "${RED}Error ocured while repairing $mailbox!${NORM}" |tee /dev/stderr |tee >(logg)
+      $silent || echo -e "$MSGT mailbox # ${count}: ${CYA}$mailbox${NORM} ... " |tee >(logg)
     fi
     if [ -n "$output" ]; then
-        entries=$(grep -c 'message' <<< "$output")
-        if [ -n "$entries" ]; then
-            entries=${#output}
-	    echo -e "${RED}Found $entries problematic objects.${NORM}" |tee >(logg)
-        fi
-        if $verbose; then
-            echo -e "${BLUE}Verbose mode active. Dumping result:${NORM}" |tee >(logg)
-            echo "$output"
-        fi
+      entries=$(grep -c 'message' <<< "$output")
+      if [ -n "$entries" ]; then
+        entries=${#output}
+        $silent && echo -e "$MSGT mailbox # ${count}: ${CYA}$mailbox${NORM} ... " |tee >(logg)
+        echo -ne "${RED}Found $entries problematic objects.${NORM}\n" |tee >(logg)
+      fi
+      if $verbose; then
+        echo -e "${BLUE}Verbose mode active. Dumping result:${NORM}" |tee >(logg)
+        echo "$output"
+      fi
     else
-        echo "No problematic objects detected." |tee >(logg)
+      $verbose && echo -e "No problematic objects detected.\n" |tee >(logg)
     fi
-    echo
     #
-done
-echo -e "${CYA}${count} mailboxes checked.${NORM}" |tee >(logg)
-# check errors
-if [[ $errors -gt 0 ]]; then
+  done
+  echo -e "${CYA}${count} mailboxes checked.${NORM}" |tee >(logg)
+  # check errors
+  if [[ $errors -gt 0 ]]; then
     echo -e "${RED}$errors errors encountered!${NORM}" |tee /dev/stderr |tee >(logg)
-fi
-# directory not found errors
-if [[ $err_dir -gt 0 ]]; then
+  fi
+  # directory not found errors
+  if [[ $err_dir -gt 0 ]]; then
     echo -e "${RED}$err_dir directories not found!${NORM}" |tee /dev/stderr |tee >(logg)
-fi
-echo -e "The log file is: $log"
-$repair && echo -e "${CYA}Please note: Run the script until no more errors occur.${NORM}" |tee >(logg)
-#
-echo "Script $0 stopped at: $(date +%Y%m%d-%H%M%S%n)" >> "$log"
+  fi
+  echo -e "The log file is: $log"
+  $repair && echo -e "${CYA}Please note: Run the script until no more errors occur.${NORM}" |tee >(logg)
+  #
+  echo "Script $0 stopped at: $(date +%Y%m%d-%H%M%S%n)" >> "$log"
 
 #
 # eof
