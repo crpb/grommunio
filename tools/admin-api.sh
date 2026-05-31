901f9e22a937ce10d27721e94a6a7e084c12145a
Christopher Bock <christopher@bocki.com>
2026-05-31 02:55:14 +0200

    admin-api example

--- tools/admin-api.sh
+++ tools/admin-api.sh
@@ -1,14 +1,24 @@
 #!/usr/bin/env bash
 
-cleanup() { rm -rf "$COOKIE" ; }
-trap cleanup EXIT
+# https://github.com/grommunio/admin-api/issues/33#issuecomment-2467681926
+# > This can be achieved by creating a user with `SystemAdminRO` permissions and then generating an access token via `grommunio-admin user login <username> --nopass --token`.
+# > I can also add an explicit expiry parameter to the CLI call if needed, otherwise the global default for login tokens applies (`security.jwtExpiresAfter` configuration).
+# https://github.com/grommunio/admin-api/issues/33#issuecomment-3442046597
+# > You can now use --no-maildir to create a user without a mailbox.
+#
+# So to create a api-user we just do
+# `grommunio-admin user create --no-maildir someapiapp@host.tld`
+# `grommunio-admin password someapiapp@host.tld` # this is still needed
+# `grommunio-admin user login someapiapp@host.tld --nopass --token`
+# Still waiting for better parsable output for the last one.
+# https://github.com/grommunio/admin-api/issues/66
 
-COOKIE=$(mktemp -t adminapi-XXXXXX)
-if test -z $GROMHOST; then
+
+if test -z "$GROMHOST"; then
   printf 'export GROMHOST=grommunio.foo.tld to skip this question.\n\n'
   read -p "Enter Hostname: " -r GROMHOST
 fi
-if test -z $APIPASS; then
+if test -z "$APIPASS"; then
   printf 'export ADMINPASS=foo to skip this question.\n\n'
   read -p "Enter Adminpassword: " -r APIPASS
 fi
@@ -27,15 +37,3 @@ read -r -d '' CURLOPTS << EOOPTS
 EOOPTS
 
 curl -v $CURLOPTS https://$GROMHOST:8443/api/v1/system/orgs
-
-
-  # -H 'Content-Type:application/json'
-  # -H 'Accept:application/json'
-  #-H \"X-Auth-Token: $JWT\" \
-  #-H \"X-CSRFToken: $CSRF\" \
-  #-H \"Cookie: csrftoken=$CSRF\" \
-
- # -H 'Cookie: csrftoken=$CSRF'
- # -H 'Token:$JWT'
- # -H 'X-Auth-Token:$JWT'
-  # -d \"user=admin&pass=$APIPASS\" \
